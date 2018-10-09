4ad36e532287388e9823c23e5314a3a56167578c
Pokepunch <Pokepunch@users.noreply.github.com>
2018-10-09 12:32:15 +0100

    First Commit
    Office door is in Unity with some basic character controls.
    
    No Interactions yet.

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterController controller;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Get the players movement for this frame
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.SimpleMove(movement * speed);
+
+        Vector2 view = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    }
+}
