b2783bb1f990716fce5e3dca61292a71c0aff373
Pokepunch <Pokepunch@users.noreply.github.com>
2018-11-06 17:39:57 +0000

    Base Office finished, Lighting looks sweet

--- Assets/Code/OfficeDoorOpenScript.cs
+++ Assets/Code/OfficeDoorOpenScript.cs
@@ -7,13 +7,23 @@ public class OfficeDoorOpenScript : MonoBehaviour {
     public Animator anim;
     private bool isClosed;
 
+    public bool isLocked;
+
     private void Start()
     {
         isClosed = anim.GetBool("IsClosed");
+        if (isLocked)
+        {
+            anim.SetBool("IsLocked", true);
+        }
     }
 
     public void Activate()
     {
+        if (isLocked)
+        {
+            anim.Play("Locked");
+        }
         if (isClosed)
         {
             anim.SetBool("IsClosed", false);
